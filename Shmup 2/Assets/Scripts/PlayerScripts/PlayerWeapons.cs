4ad536e4b8f0d070d7d63d7c7fcc902e145e9581
Natasha Amy Astra <NatashaAmyAstra@gmail.com>
2024-10-01 12:10:35 +0200

    Added projectile base behaviour
    Created player projectile

--- Shmup 2/Assets/Scripts/PlayerScripts/PlayerWeapons.cs
+++ Shmup 2/Assets/Scripts/PlayerScripts/PlayerWeapons.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerWeapons : MonoBehaviour
 {
     [SerializeField] private float _shootDelaySeconds = 0.5f;
+    [SerializeField] private GameObject _projectilePrefab;
+    [SerializeField] private Transform _projectileOrigin;
     private float _timeSinceLastShot = 0;
 
     private void Awake() {
@@ -11,14 +13,20 @@ public class PlayerWeapons : MonoBehaviour
     }
 
     private void Update() {
+        // count every frame how long it has been since the last shot was fired
         _timeSinceLastShot += Time.deltaTime;
+
+        // if the player is holding the shoot input, fire as soon as the delay is over
         if(InputManager.IsFiring && _timeSinceLastShot >= _shootDelaySeconds)
         {
             FirePrimary();
+            _timeSinceLastShot = 0;
         }
     }
 
     private void FirePrimary() {
+        // spawn a bullet at projectile origin
+        Instantiate(_projectilePrefab, _projectileOrigin.position, Quaternion.identity);
     }
 
     private void FireSpecial() {
--- Shmup 2/Assets/Scripts/Weapons/Projectile.cs
+++ Shmup 2/Assets/Scripts/Weapons/Projectile.cs
@@ -3,16 +3,33 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _borderDestroyMargin = 1;
     [SerializeField] private int _damage = 1;
 
+    private float _screenBorder;
+
+    private void Awake() {
+        // calculate the height of the screen to know when to delete the projectile
+        _screenBorder = Camera.main.ViewportToWorldPoint(Vector3.up).y;
+        Debug.Log(_screenBorder);
+    }
+
     private void Update() {
+        // move projectile forward each frame, delete it if it passes the screen border
         transform.position += transform.up * _speed * Time.deltaTime;
+
+        if(Mathf.Abs(transform.position.y) > _screenBorder + _borderDestroyMargin)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter(Collision collision) {
+        // if projectile collides with a damagable object, deal damage to it and remove projectile
         if(collision.gameObject.TryGetComponent(out IDamagable damagable))
         {
             damagable.Damage(_damage);
+            Destroy(gameObject);
         }
     }
 }
